dbd2b166122b5b7311c6ff7502607e3adb58d501
bhanuprakash <bhanuprakash@10.183.245.162>
2021-09-24 22:34:42 -0500

    Green Light |

--- dodoku/test/infotest.py
+++ dodoku/test/infotest.py
@@ -8,7 +8,7 @@ class infotest(unittest.TestCase):
     def test_info_010shouldreturnmyusername(self):
         expectedresult = {"info:U can complete this assignment"}
         parms = {'op':'info'}
-        actualresult=info._info(parms)
-        self.assertDictequal(expectedresult , actualresult)
+        actualresult = info._info(parms)
+        self.assertDictEqual(expectedresult , actualresult)
 
 
