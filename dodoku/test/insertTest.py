a0b3306cca545e96a33e47e8cec5cbb47552993b
bhanuprakash <bhanuprakash@172.19.42.58>
2021-10-21 02:40:41 -0500

    Red Light |

--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -5,7 +5,7 @@ class InsertTest(TestCase):
       
     def test_create_010shouldreturnmyusername(self):
         grid= [0,-6,0,0,0,0,0,-5,-9,-9,-3,0,-4,-8,0,0,0,0,0,0,0,0,0,-7,-3,0,0,0,-5,0,0,-1,0,0,-4,-6,0,0,0,0,0,-6,0,-9,0,0,-8,-1,-2,0,0,0,0,0,0,0,0,0,-7,0,0,0,0,-8,0,-4,0,0,-1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0,0,-7,0,-5,-8,0,0,0,-2,0,0,0,-6,-1,0]
-        expectedresult = {'grid': grid,'status':'ok' , 'integrity':'6fcd71ef7722e7573d2f607a35cfa48f72b03c4cea135ac31f7ef73a58e50a8a'}
+        expectedresult = {'grid': grid,'status':'ok' , 'integrity':'a'}
         parms = {'op' : 'insert','value' : '2','cell':'r3c4'}
         actualresult = insert._insert(parms)
         if expectedresult['grid'] == actualresult['grid'] and expectedresult['status'] == actualresult['status']and actualresult['integrity'] in expectedresult['integrity'] :
